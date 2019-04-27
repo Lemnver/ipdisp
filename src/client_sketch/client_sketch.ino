01f2f8b0a059d8284178d751e040b6fb6cfc7e29
lemnver <lemnver@github.com>
2019-04-27 13:43:31 +0200

    client server finished, kind of

--- src/client_sketch/client_sketch.ino
+++ src/client_sketch/client_sketch.ino
@@ -1,10 +1,28 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+String sentString = "";
+char data;
 void setup(){
+  Serial.begin(9600);
+  
   lcd.begin(16,2);
-  lcd.print("uh uh");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 0);
+  lcd.print("waiting");
+  lcd.setCursor(0, 0);
+
 }
 void loop(){
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    sentString += c;
+    if (sentString.length() == 15){
+      lcd.clear();
+      lcd.print(sentString);
+      sentString == "";
+    }
+  }
+  Serial.write("ping\n");
+  
 }
+
 
--- src/server.py
+++ src/server.py
@@ -1,8 +1,11 @@
 import subprocess
 import os
 import time
+import sys
+import serial
 
 def checkCurl():
+    # check if package curl is available
     check = subprocess.call(['which', 'curl'], stdout=open(os.devnull,"wb"))
 
     if check == 0:
@@ -11,6 +14,7 @@ def checkCurl():
         return False
 
 def get_ip():
+    # run curl on the adress icanhazip.com for ip
     process_args = ['curl', 'icanhazip.com']
     result = subprocess.Popen(process_args, stdout=subprocess.PIPE, stderr=open(os.devnull, "wb"))
     ipbin = result.communicate()[0]
@@ -20,32 +24,38 @@ def get_ip():
     return ip
 
 def checkArduino():
-    # check if arduino with client code is connected
-    # returns whatever object for arduino on success
+    try:
+        arduino = serial.Serial(path, baud, timeout=10)
+    except Exception as e:
+        return False
+    print("arduino connected")
+    time.sleep(2)
+    ip = get_ip()
+    arduino.write(get_ip().encode('ascii'))
+    time.sleep(1)
+    
+    # loop untill arduino is disconnected
+    try:
+        while arduino.read():
+            time.sleep(2)
+    except Exception as e:
+        print("arduino disconnected")
+        arduino.close()
+        return False
 
-    # conditions
-    # ...
-
+    print("arduino disconnected")
+    arduino.close()
     return False
 
-def send_ip():
-    # send ip to arduino
-    # return result
-
-    return False
-
-def handleArduino(ard):
-    # get ip, send to arduino
-    # return once disconnected 
 
 def main():
+    if not checkCurl():
+        print("Error, curl not installed");
+        sys.exit(1)
+    
     while True:
-        res = checkArduino()
-        if res:
-            handleArduin(res)
-        else:
-            # might change this later depending on how usb works
-            time.sleep(4)
-        
+        arduino = checkArduino()
+        time.sleep(1)
 
-
+if __name__ == '__main__':
+    main()
